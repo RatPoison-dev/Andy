1c84baed25554bdb0c1f2359851f7973ff1a9b00
retart1337 <misha.domanov@mail.ru>
2020-07-26 13:22:47 +0300

    Update misc.js

--- commands/misc.js
+++ commands/misc.js
@@ -4,7 +4,7 @@ let commands = {
     help: async (message) => {
         let info = await database.getGuildInfo(message.guild.id)
         let prefix = info.prefix
-        message.channel.send(`Current prefix: ${prefix}\nCommands:\nmonitor [link] - add account to bans monitor\nsetChannel [channel] - channel for bans monitor\nsetPrefix [newPrefix] - set a prefix for bot`)
+        message.channel.send(`Current prefix: ${prefix}\nCommands:\nmonitor [link] - add account to bans monitor\nsetChannel [channel] - channel for bans monitor\nsetPrefix [newPrefix] - set a prefix for bot\ntags - get a help message for tags`)
     }
 }
 
